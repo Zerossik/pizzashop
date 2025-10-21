9ae722bf2774bd487c7c816d258e1a7eed5f6708
Zerossik <zeross19940303@gmail.com>
2025-10-21 21:43:58 +0300

    modal update

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,18 @@
 import { useState } from "./hooks/useState.js";
 
 export class Modal {
+  #defaultOptions = { title: "", closeBtn: null, renderTarget: "#modal-root" };
   #state;
   #modalInner;
   #content;
-
-  constructor(title = "", selector = ".modal") {
-    this.BASE_SELECTOR = selector;
+  constructor(selector = "modal", options) {
+    this.BASE_SELECTOR = selector.replace(/[^a-zA-Z0-9_-]/g, "");
+    // ТУТ должна вызваться виладатор настройки
+    this.options = Object.assign(this.#defaultOptions, options);
 
     const initialState = {
       isOpen: false,
-      title: title,
+      title: this.options.title,
     };
 
     this.#state = useState(initialState, (target, prop, value) => {
@@ -18,11 +20,21 @@ export class Modal {
     });
 
     this.#createModal();
+    this.attachEvents();
 
     //   ELEMENTS
-    this.#modalInner = this.modal.querySelector(`${this.BASE_SELECTOR}__inner`);
-    this.title = this.modal.querySelector(`${this.BASE_SELECTOR}__title`);
-    this.modalRoot = document.getElementById("modal-root");
+
+    this.#modalInner = this.modal.querySelector(
+      `.${this.BASE_SELECTOR}__inner`
+    );
+    this.modalTitle = this.modal.querySelector(`.${this.BASE_SELECTOR}__title`);
+
+    this.modalRoot = document.querySelector(this.options.renderTarget);
+    if (!this.modalRoot) {
+      throw new Error(
+        `Render target ${this.options.renderTarget} not found in DOM`
+      );
+    }
   }
 
   attachEvents() {
@@ -31,52 +43,65 @@ export class Modal {
 
   #handleClick = (e) => {
     const clickedElement = e.target;
-
-    if (clickedElement.matches(".modal__close-btn")) {
-      console.log(clickedElement);
+    if (clickedElement.matches(`.${this.BASE_SELECTOR}__close-btn`)) {
       this.close();
     }
   };
 
   show(htmlContent) {
-    if (htmlContent) this.#content = htmlContent;
+    if (htmlContent && !(htmlContent instanceof HTMLElement))
+      throw new TypeError("htmlContent must be an HTMLElement");
+    this.#content = htmlContent;
     this.#state.isOpen = true;
-    return this.#state.isOpen;
+    return this;
   }
 
   close() {
+    this.#content = null;
+    this.#state.isOpen = false;
     this.modal.remove();
+  }
 
-    return this.#state.isOpen;
+  setContent(newContent, isClearMOdal = false) {
+    if (!(newContent instanceof HTMLElement))
+      throw new TypeError("Content must be HTMLElement");
+
+    this.#content = newContent;
+    this.#render();
   }
 
   #createModal() {
     this.modal = document.createElement("div");
     this.modal.classList.add(this.BASE_SELECTOR);
-    this.attachEvents();
 
     this.modal.innerHTML = `
-      <div class="modal__backdrop">
-        <div class="modal__content">
-          <div class="modal__header">
-            <p class="modal__title">${this.#state.title}</p>
-            <button class="modal__close-btn">X</button>
-          </div>
-          <div class="modal__inner"></div>
+      <div class="${this.BASE_SELECTOR}__backdrop">
+        <div class="${this.BASE_SELECTOR}__content">
+          <div class="${this.BASE_SELECTOR}__header">
+            <p class="${this.BASE_SELECTOR}__title">${this.#state.title}</p>
+              <button class="${this.BASE_SELECTOR}__close-btn">X</button>
+            </div>
+          <div class="${this.BASE_SELECTOR}__inner"></div>
         </div>
       </div>`;
   }
 
+  set title(title) {
+    if (!title || typeof title !== "string")
+      throw new TypeError("title must be a string");
+    this.#state.title = title;
+  }
+
   #render() {
+    this.modalTitle.textContent = this.#state.title;
+
     if (this.#content instanceof HTMLElement) {
       this.#modalInner.appendChild(this.#content);
     }
 
-    this.modalRoot.appendChild(this.modal);
+    if (this.#state.isOpen && !this.modalRoot.contains(this.modal)) {
+      this.#modalInner.innerHTML = "";
+      this.modalRoot.appendChild(this.modal);
+    }
   }
 }
-
-const modal = new Modal();
-const div = document.createElement("div");
-div.innerHTML = "HELLO";
-modal.show(div);
