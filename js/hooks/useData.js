4f563ba90784e0f3166c21ba680d9f731487b371
Zerossik <zeross19940303@gmail.com>
2025-10-30 16:10:31 +0200

    Update useData.js

--- js/hooks/useData.js
+++ js/hooks/useData.js
@@ -1,5 +1,11 @@
 export const useData = async (path) => {
-  const res = await fetch(path);
+  const baseURL =
+    window.location.hostname === "https://zerossik.github.io"
+      ? "https://zerossik.github.io"
+      : "";
+  console.log(baseURL + path);
+
+  const res = await fetch(baseURL + path);
   if (!res.ok) throw new Error("Data not found");
   const data = await res.json();
   return data;
